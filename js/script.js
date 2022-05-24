463bdcb830e01b55a1cecc7bea96836ca30131d8
jhwmac <jkr2505@naver.com>
2022-05-24 13:07:29 +0900

    slide 반응형

--- js/script.js
+++ js/script.js
@@ -4,25 +4,67 @@ $(document).ready(function () {
     let nav = $('.nav');
     let headerMain = $('.header-main');
     let logo = $('.logo');
-    nav.mouseenter(function(){
+    nav.mouseenter(function () {
         $(this).addClass('nav-active');
     });
-    headerMain.mouseleave(function(){
+    headerMain.mouseleave(function () {
         nav.removeClass('nav-active');
     });
-    logo.mouseenter(function(){
+    logo.mouseenter(function () {
         nav.removeClass('nav-active');
     });
 
     // 스크롤 기능
     let header = $('.header')
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         let temp = $(window).scrollTop();
-        if(temp > 0) {
+        if (temp > 0) {
             header.addClass('fix-active')
-        }else{
+        } else {
             header.removeClass('fix-active')
         }
     });
 
+    // 모바일 메뉴
+    let mBt = $('.m-bt');
+    let mMenuBg = $('.m-menu-bg');
+    let mMenuWrap = $('.m-menu-wrap');
+    let mBtClose = $('.m-bt-close');
+
+    mBt.click(function () {
+        mMenuWrap.addClass('m-menu-wrap-active');
+        mMenuBg.addClass('m-menu-bg-active');
+    });
+    mBtClose.click(function () {
+        mMenuWrap.removeClass('m-menu-wrap-active');
+        mMenuBg.removeClass('m-menu-bg-active');
+    });
+
+    mMenuBg.click(function () {
+        mMenuWrap.removeClass('m-menu-wrap-active');
+        mMenuBg.removeClass('m-menu-bg-active');
+    });
+
+    // 화면 리사이징 처리
+    $(window).resize(function () {
+        let w = $(window).width();
+        if (w > 700) {
+            mMenuWrap.removeClass('m-menu-wrap-active');
+            mMenuBg.removeClass('m-menu-bg-active');
+        }
+    });
+
+
+
 });
+
+window.onload = function () {
+    // visual slide
+    let swVisual = new Swiper('.sw-visual', {
+        loop: true,
+        navigation: {
+            prevEl: '.sw-visual-prev',
+            nextEl: '.sw-visual-next'
+        }
+    });
+}
